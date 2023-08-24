cf20383516ce3cb19ab565bbf950edc62b6f7188
venkateshreddy2003 <venkateshreddy2003@outlook.com>
2023-08-24 22:51:53 +0530

    completed section list task

--- App.tsx
+++ App.tsx
@@ -1,110 +1,79 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  RefreshControl,
+  FlatList,
+  SectionList,
+} from 'react-native';
 
 const App = () => {
+  const [Sections, setSections] = useState([
+    {
+      title: 'Title 1',
+      data: ['Item 1-1', 'Item 1-2'],
+    },
+  ]);
+  const onRefresh = () => {
+    setRefreshing(true);
+    const adding_index = Sections.length + 1;
+    setSections([
+      ...Sections,
+      {
+        title: 'Title ' + adding_index,
+        data: ['Item ' + adding_index + '-1', 'Item ' + adding_index + '-2'],
+      },
+    ]);
+    setRefreshing(false);
+  };
+  const [Refreshing, setRefreshing] = useState(false);
+
   return (
-    <View style={styles.body}>
-      <View style={styles.row}>
-        <View style={styles.view1}>
-          <Text style={styles.text}>1</Text>
+    <SectionList
+      keyExtractor={(item, index) => index.toString()}
+      sections={Sections}
+      renderItem={({item}) => (
+        <View style={styles.item}>
+          <Text style={styles.text_item}>{item}</Text>
         </View>
-        <View style={styles.view2}>
-          <Text style={styles.text}>2</Text>
+      )}
+      renderSectionHeader={({section}) => (
+        <View style={styles.header}>
+          <Text style={styles.text_header}>{section.title}</Text>
         </View>
-        <View style={styles.view3}>
-          <Text style={styles.text}>3</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view4}>
-          <Text style={styles.text}>4</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view5}>
-          <Text style={styles.text}>5</Text>
-        </View>
-      </View>
-      <View style={styles.big_row}>
-        <View style={styles.view6}>
-          <Text style={styles.text}>6</Text>
-        </View>
-        <View style={styles.view7}>
-          <Text style={styles.text}>7</Text>
-        </View>
-      </View>
-    </View>
+      )}
+      refreshControl={
+        <RefreshControl refreshing={Refreshing} onRefresh={onRefresh} />
+      }
+    />
   );
 };
 
 const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  row: {
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
+  header: {
+    backgroundColor: '#4ae1fa',
     justifyContent: 'center',
-  },
-  big_row: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-  view1: {
-    flex: 1,
-    backgroundColor: '#00ffff',
     alignItems: 'center',
-    justifyContent: 'center',
+    borderWidth: 2,
   },
-  view2: {
-    flex: 2,
-    backgroundColor: '#ff00ff',
+  item: {
+    borderBottomWidth: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  view3: {
-    flex: 3,
-    backgroundColor: '#ffff00',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view4: {
-    flex: 1,
-    backgroundColor: '#ff0000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view5: {
-    flex: 1,
-    backgroundColor: '#0fff0f',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view6: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view7: {
-    flex: 1,
-    backgroundColor: '#0000ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
+  text_header: {
     color: '#000000',
-    fontSize: 35,
+    fontSize: 45,
     fontStyle: 'italic',
     margin: 10,
+  },
+  text_item: {
+    color: '#000000',
+    fontSize: 35,
+    margin: 5,
   },
 });
 
