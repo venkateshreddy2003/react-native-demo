7223046c757b9c0d192bbd12447e2741f82f4010
venkateshreddy2003 <venkateshreddy2003@outlook.com>
2023-08-24 06:02:28 +0530

    Completed useState

--- App.tsx
+++ App.tsx
@@ -1,23 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import {Button, Linking, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 function App(): React.JSX.Element {
+  const [name, setName] = useState('Mash');
+  const [session, setSession] = useState({number: 6, title: 'state'});
+  const [current, setCurrent] = useState(true);
+
+  const onClickHandler = () => {
+    setName('Programming with Venkatesh');
+    setSession({number: 7, title: 'Style'});
+    setCurrent(false);
+  };
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Venkatesh reddy</Text>
-      <Button
-        title="Link"
-        onPress={() => {
-          Linking.openURL('https://www.google.com');
-        }}
-      />
+      <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>
+        This is session number {session.number} and about {session.title}
+      </Text>
+      <Text style={styles.text}>
+        {current ? 'current session' : 'next session'}
+      </Text>
+      <Button title="Update State" onPress={onClickHandler} />
     </View>
   );
 }
